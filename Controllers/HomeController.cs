1b6d112a2b6761c1038eb6d5ade4834287f15add
hungminh0907 <hungminh0907@gmail.com>
2023-12-24 21:11:18 +0700

    Error json image từ data bị null

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using WebPhoneEcommerce.Models;
+using WebPhoneEcommerce.Models.Curd;
 using WebPhoneEcommerce.Models.Entity;
 
 namespace WebPhoneEcommerce.Controllers
@@ -10,18 +12,54 @@ namespace WebPhoneEcommerce.Controllers
         private readonly PhoneEcommerceContext _context;
 
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        private readonly string _apiHost = "";
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(PhoneEcommerceContext context, IConfiguration config)
         {
-            _logger = logger;
+            _context = context;
+            _config = config;
+            _apiHost = config.GetValue<string>("UrlHost:ApiHost");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-           
-            return View();
+            var Items = await getCurd();
+            return View(Items);
         }
+        public async Task<List<ViewModelCurd>> getCurd()
+        {
+            string Url = "https://localhost:7007/api/ApiCurd/Show-danh-sach";
+            //string Url = _apiHost + @"api/ApiCurd/Show-danh-sach";
 
+            using (var client = new HttpClient())
+            {
+                var res = await client.GetAsync(Url);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var ViewlistItem = new List<ViewModelCurd>();
+
+                    var ListItem = res.Content.ReadAsAsync<List<ResultApi>>().Result;
+                    foreach (var item in ListItem)
+                    {
+                        ViewModelCurd ShowList = new ViewModelCurd();
+                        ShowList.Id = item.Id;
+                        ShowList.ProductId = item.ProductId;
+                        ShowList.ProductName = item.ProductName;
+                        ShowList.Description = item.Description;
+                        ShowList.UnitPrice = item.UnitPrice;
+
+                        ShowList.Urlimg = JsonConvert.DeserializeObject<List<ResultApiImg>>(item.Urlimg);
+
+
+                        ViewlistItem.Add(ShowList);
+                    }
+                    return ViewlistItem;
+                }
+            }
+            return null;
+        }
         public IActionResult Privacy()
         {
             return View();
