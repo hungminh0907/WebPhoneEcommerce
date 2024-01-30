08b85c22f01809cdc5936df7e9bb73ad6356e5c6
Duc Phuong Nguyen <phuongngdc@gmail.com>
2024-01-30 13:48:10 +0700

    updateSP & deleteSP in HomeAdminController

--- Areas/Admin/Controllers/HomeAdminController.cs
+++ Areas/Admin/Controllers/HomeAdminController.cs
@@ -7,6 +7,7 @@ using WebPhoneEcommerce.Common;
 using WebPhoneEcommerce.Models.Curd;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
+using System.ComponentModel;
 
 namespace WebPhoneEcommerce.Areas.Admin.Controllers
 {
@@ -19,6 +20,7 @@ namespace WebPhoneEcommerce.Areas.Admin.Controllers
         private readonly PhoneEcommerceContext _context;
         private readonly IConfiguration _config;
         private readonly string _apiHost = "";
+        private static string wwwroot = Directory.GetCurrentDirectory() + "\\wwwroot";
         public HomeAdminController(PhoneEcommerceContext context, IConfiguration config)
         {
             _context = context;
@@ -104,14 +106,54 @@ namespace WebPhoneEcommerce.Areas.Admin.Controllers
             }
             return View();
         }
+
+
         [Route("Cap-Nhat")]
         public IActionResult updateSP(string id)
         {
             var item = _context.Products.FirstOrDefault(x => x.Id == id);
             return View(item);
         }
+
+
         [Route("Cap-Nhat")]
-        [HttpPost]
+        [HttpPut]
+        public async Task<IActionResult> updateSP(string id, UpdateProduct input)
+        {
+            //string Url = "https://localhost:7007/api/ApiCurd/Show-danh-sach";
+            string Url = _apiHost + @"api/ApiCurd/cap-nhat-san-pham" + "/" + id;
+
+            using (var client = new HttpClient())
+            {
+                var data = new MultipartFormDataContent();
+                data.Add(new StringContent(input.ProductId), "ProductId");
+                data.Add(new StringContent(input.ProductName), "ProductName");
+                data.Add(new StringContent(input.Description), "Description");
+                data.Add(new StringContent(input.UnitPrice.ToString()), "UnitPrice");
+                data.Add(new StringContent(input.ProductId + " " + input.ProductName), "Filter");
+
+                var listimg = new List<string>();
+                foreach (var img in input.Urlimg)
+                {
+                    var imgPath = UploadFiles.SaveImage(img);
+                    listimg.Add(imgPath);
+                    var imgStream = new MemoryStream(System.IO.File.ReadAllBytes(wwwroot + imgPath));
+                    var imgContent = new ByteArrayContent(imgStream.ToArray());
+                    data.Add(imgContent, "Images", img.FileName);
+                }
+
+                var res = await client.PutAsync(Url, data);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("DanhSach");
+                }
+            }
+
+            return null;
+        }
+
+        /*
         public IActionResult updateSP(string id, string ProductId, string ProductName, string Description, decimal UnitPrice, IFormFile hinhanh)
         {
             var item = _context.Products.FirstOrDefault(x => x.Id == id);
@@ -130,9 +172,27 @@ namespace WebPhoneEcommerce.Areas.Admin.Controllers
             return RedirectToAction("DanhSach");
 
         }
-
+        */
 
         [Route("Xoa")]
+
+        public async Task<IActionResult> deleteSP(string id)
+        {
+            //string Url = "https://localhost:7007/api/ApiCurd/Show-danh-sach";
+            string Url = _apiHost + @"api/ApiCurd/xoa-san-pham"+"/"+id;
+
+            using (var client = new HttpClient())
+            {
+                var res = await client.DeleteAsync(Url);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("DanhSach");
+                }
+            }
+            return null;
+        }
+        /*
         public IActionResult deleteSP(string id)
         {
             var item = _context.Products.FirstOrDefault(x => x.Id == id);
@@ -155,6 +215,7 @@ namespace WebPhoneEcommerce.Areas.Admin.Controllers
 
             return RedirectToAction("DanhSach");
         }
+        */
 
 
     }
